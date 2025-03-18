c0e6998ea4037ef9d5e678e78520e7c4224e0e1e
wiki73 <vladimir.savchenko.an@gmail.com>
2025-03-18 22:45:29 +0300

    Добавлена возможность загружать и отображать фотографии

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+UPLOAD_FOLDER = 'galery_mars/static/img'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
 def gallery():
-    # Список изображений для галереи
-    images = [
-        'img/mars1.jpg',
-        'img/mars2.jpg',
-        'img/mars3.jpg'
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            return redirect(url_for('gallery'))
+
+    standard_images = [
+        'mars1.jpg',
+        'mars2.jpg',
+        'mars3.jpg'
     ]
+    
+    uploaded_images = os.listdir(app.config['UPLOAD_FOLDER'])
+    
+    images = standard_images + uploaded_images
     return render_template('gallery.html', images=images)
 
 if __name__ == '__main__':
